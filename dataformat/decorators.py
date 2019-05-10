b62f1e72dece373a52753d48a07b6e6f069e591e
Adrian Tomasov <adiosnb@gmail.com>
2019-05-10 21:26:55 +0200

    [readonly] adding decorators which keep safe __setattr__ method

--- dataformat/decorators.py
+++ dataformat/decorators.py
@@ -1,9 +1,12 @@
+from functools import wraps
+
 from dataformat.exceptions import DataFormatReadOnlyException
 
 
 def readonly_check(func):
     attr = 'readonly'
 
+    @wraps(func)
     def inner(instance, *args, **kwargs):
         if getattr(instance, attr):
             raise DataFormatReadOnlyException
@@ -11,3 +14,16 @@ def readonly_check(func):
             func(instance, *args, **kwargs)
 
     return inner
+
+
+def setattr_readonly_check(cls):
+    attr = 'readonly'
+
+    # TODO think about wraps for class
+    class Wrapped(cls):
+        def __setattr__(self, key, value):
+            if hasattr(self, attr) and getattr(self, attr):
+                raise DataFormatReadOnlyException
+            else:
+                super().__setattr__(key, value)
+    return Wrapped
