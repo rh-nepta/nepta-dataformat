0160e72601e276386655ad434588319d4843f667
Adrian Tomasov <atomasov@redhat.com>
2019-06-13 11:50:49 +0200

    [utils] adding versioneer

--- dataformat/__init__.py
+++ dataformat/__init__.py
@@ -1,3 +1,6 @@
 from dataformat.package import DataPackage
 from dataformat.attachments import Types as AttachmentTypes
 from dataformat.package import FileFlags
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
