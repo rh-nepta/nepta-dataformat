5e63c037762c06dd875264e582e59d3d3cd3ea82
Adrian Tomasov <atomasov@redhat.com>
2019-07-23 17:17:14 +0200

    [init] adding Section into API

--- nepta/dataformat/__init__.py
+++ nepta/dataformat/__init__.py
@@ -1,6 +1,8 @@
 from nepta.dataformat.package import DataPackage
 from nepta.dataformat.attachments import Types as AttachmentTypes
 from nepta.dataformat.package import FileFlags
+from nepta.dataformat.section import Section
+
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
